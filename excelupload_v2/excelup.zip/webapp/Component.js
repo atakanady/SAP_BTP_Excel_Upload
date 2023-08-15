468ffc207795c83bf032874504641788b28e0171
Atakan <79115077+atakanady@users.noreply.github.com>
2023-08-15 09:36:08 +0300

    +

---
+++ excelupload_v2/excelup.zip/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("excelup.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
